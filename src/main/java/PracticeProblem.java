a149b98905c0b4ae2f71a7acb610339e58cf7d48
ilakkiyan-uruthireswaran-ics4u1-3-25 <Ilakkiyan.uruthireswaran@student.tdsb.on.ca>
2025-03-17 21:23:59 +0000

    passed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,20 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static int goodString(String str) {
+		int n = str.length();
+		int min = Integer.MAX_VALUE;
 
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (str.charAt(i) == str.charAt(j)) {
+					min = Math.min(min, i + (n - 1 - j));
+				}
+			}
+		} return (min == Integer.MAX_VALUE) ? -1 : min;
+		}
+	public static void main(String args[]) {
+		int result = goodString("pqr");
+		System.out.println(result);
 	}
 
 }
